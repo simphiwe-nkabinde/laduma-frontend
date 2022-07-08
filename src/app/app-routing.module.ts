4364c3b21c9f1babd665e575f5e799f22737d5c2
DA_Learner_29 <ds_intern@DS-LEARNERs-MacBook-Air.local>
2022-07-08 08:26:59 +0200

    add routing for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatchPageComponent } from './pages/match-page/match-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'match/:id', component: MatchPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
