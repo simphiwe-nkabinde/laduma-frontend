05a584d9f012aa48e345783e49a7cad162a50c8b
Monoametsi <216026633@student.uj.ac.za>
2022-07-08 11:09:17 +0200

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatchPageComponent } from './pages/match-page/match-page.component';
 import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MatchPageComponent,
-    TabMenuComponent
+    TabMenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
