1da7818d4c0761426fcfa55d21257a0bb3abdafa
Monoametsi <216026633@student.uj.ac.za>
2022-07-08 15:43:26 +0200

    add changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { MatchPageComponent } from './pages/match-page/match-page.component';
 
 const routes: Routes = [
-  { path: 'match/:id', component: MatchPageComponent}
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  { path: 'match/:id', component: MatchPageComponent},
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
