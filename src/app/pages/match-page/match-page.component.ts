057bc13dfc61bada9ef8cde992ed9f5cd4f64c25
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-07-12 11:02:50 +0200

    get and display team names for a single match with Match_ID

--- src/app/pages/match-page/match-page.component.ts
+++ src/app/pages/match-page/match-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatchService } from 'src/app/services/match.service';
+import { filterObject } from 'src/app/utility/utilities';
 
 @Component({
   selector: 'app-match-page',
@@ -7,11 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchPageComponent implements OnInit {
 
-  players: string[] = ['jacob mabuza', 'joseph sithole', 'helen goboza', 'fistos maganga']
+  homeTeam: string = '';
+  awayTeam: string = '';
+  homePlayers: any;
+  awayPlayers: any;
 
-  constructor() { }
+  constructor(
+    private matchService: MatchService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+
+    this.matchService.getAll()
+    .subscribe(res => {
+      //get match id from url params
+      let matchId = this.route.snapshot.paramMap.get('id');
+      matchId = String(matchId);
+      
+      //filter match events by match id
+      let matchEvents = res.data.filter((property:any) => {
+        return property.attributes['Match_ID'] == matchId;
+      })
+
+      let attributes = matchEvents.map((matchEvent:any) => {
+        return matchEvent.attributes;
+      })
+
+      //team names
+      let game = attributes[0]['Game'];
+      this.homeTeam = this.getTeamsNames(game).homeTeam;
+      this.awayTeam = this.getTeamsNames(game).awayTeam;
+
+      this.homePlayers = filterObject(attributes.map((matchEvent:any) => matchEvent['Team'] = this.homeTeam), ['Player_Name'])
+      this.awayPlayers = filterObject(attributes.map((matchEvent:any) => matchEvent['Team'] = this.awayTeam), ['Player_Name'])
+    })
+  }
+
+  /**
+   * extracts the names of the home team and away team from the game string
+   * @param game eg. 'Mamelodi Sundowns vs Lamontville Golden Arrows - 6:0'
+   * @returns object of home and away team
+   */
+  getTeamsNames(game: string): {homeTeam: string, awayTeam: string} {
+    //seperator indexes
+    
+    let vsIndex = game.indexOf('vs');
+    let hyphenIndex = game.indexOf('-');
+    
+    //team names
+    const homeTeam = game.slice(0, vsIndex - 1);
+    const awayTeam = game.slice(vsIndex + 3, hyphenIndex -1)
+
+    return {
+      homeTeam: homeTeam,
+      awayTeam: awayTeam
+    }
   }
 
 }
