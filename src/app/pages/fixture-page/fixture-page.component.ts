108d2f1605a7d7a0f65295c5c4678a6cfaf47e8e
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2022-07-12 10:59:56 +0200

    get and display all matches from api

--- src/app/pages/fixture-page/fixture-page.component.ts
+++ src/app/pages/fixture-page/fixture-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchService } from 'src/app/services/match.service';
+import { filterObject } from 'src/app/utility/utilities';
 
 @Component({
   selector: 'app-fixture-page',
@@ -7,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FixturePageComponent implements OnInit {
 
-  matches: string[] = ['mamelodi vs orlando', 'k. chiefs vs g. arrows', 'mamelodi vs orlando', 'k. chiefs vs g. arrows', 'mamelodi vs orlando', 'k. chiefs vs g. arrows', 'mamelodi vs orlando', 'k. chiefs vs g. arrows',]
+  matches: any = [];
 
-  constructor() { }
+  constructor(private matchService: MatchService) { }
 
   ngOnInit(): void {
+    this.matchService.getAll()
+    .subscribe(res => {
+      let attributes: [];
+      attributes = res.data.map((matchEvent:any) => {
+        return matchEvent.attributes;
+      })
+      let filtered = filterObject(attributes, ['Game', 'Match_ID'])
+      this.matches = filtered
+    }, err => {
+      console.log(err);
+    })
   }
-
 }
